76d701a9d41199222df4c8e480fdc5f760032c26
YiLi <sciencethebird@gmail.com>
2021-10-17 04:16:58 +0800

    Update comments

--- Examples/pipe/basic_pipt.c
+++ Examples/pipe/basic_pipt.c
@@ -10,16 +10,25 @@ void err_sys(const char* x)
 }
 
 int main(){
-    int pipefd[2], n = 12;
-    char buff[100];
-    if(pipe(pipefd) < 0) err_sys("pipe error");
-    printf("read fd = %d, write fd = %d\n", pipefd[0], pipefd[1]);
+
+    char buff[100], n = 12;
+    int pipefd[2];
+
+    // create pipe
+    if(pipe(pipefd) < 0) 
+        err_sys("pipe error");
+    else
+        printf("read fd = %d, write fd = %d\n", pipefd[0], pipefd[1]);
+
+    // write data into the pipe
     if(write(pipefd[1], "hello world\n", 12) != 12)
         err_sys("write error");
-    
+
+    // read data into buffer from the pipe
     if(read(pipefd[0], buff, 100) != n)
        err_sys("read error");
 
-    write(1, buff, n);
+    // close(1);
+    write(1, buff, n); // write to stdout
     exit(0); 
 }
